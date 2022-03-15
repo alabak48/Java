737e1f07f8fb4180e11460fd89404ea8b174b9f9
Anja Labak <anja.labak12345@gmail.com>
2022-03-15 15:01:15 +0100

    Hello World!

---
+++ PredavanjaVjezbe1/src/main/java/ffos/predavanjavjezbe1/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ffos.predavanjavjezbe1;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+   //pisati nakon ove linije
+    
+    
+    } // ne pisati nakon ove linije
+   
+            
+}

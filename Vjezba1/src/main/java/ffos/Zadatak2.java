e989c60672cabcb133305c25e54bd6f5618e8b87
Anja <79693733+alabak48@users.noreply.github.com>
2022-03-26 19:15:27 +0100

    HelloWorld 100 puta odvojeno s zarezom, isti redak

---
+++ Vjezba1/src/main/java/ffos/Zadatak2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ffos;
+
+/**
+ *
+ * @author anja0
+ */
+public class Zadatak2 {
+    public static void main(String[] args) {
+        for(int i = 1;i<=100;i++){
+            // Ovdje se ne koristi system out jer će isprintat 100 dva puta
+        if(i==100){
+            System.out.print(i + ". Hello World.");
+        }else{
+             System.out.print(i + ". Hello World, ");
+        }
+                    }
+    }
+}

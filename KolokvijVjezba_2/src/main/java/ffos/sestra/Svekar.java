eac582d27b5d063660b58667329c4280d5142e4b
Anja Labak <anja.labak12345@gmail.com>
2022-04-11 20:49:35 +0200

    Getter and Setter

--- KolokvijVjezba_2/src/main/java/ffos/sestra/Svekar.java
+++ KolokvijVjezba_2/src/main/java/ffos/sestra/Svekar.java
@@ -10,5 +10,68 @@ package ffos.sestra;
  * @author anja0
  */
 public class Svekar {
+    //Za entitet Svekar potrebno je definirati sljedeća svojstva: 
+    //šifra(znakovni tip), 
+    //čarape (znakovni tip), 
+    //gustoća (decimalni broj), 
+    //kratka majica (znakovni tip), 
+    //stil frizure (znakovni tip) i 
+    //boja očiju (znakovni tip) (10).
+    
+    private String sifra;
+    private String carape;
+    private double gustoca;
+    private String kratkaMajica;
+    private String stilFrizure;
+    private String bojaOciju;
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getCarape() {
+        return carape;
+    }
+
+    public void setCarape(String carape) {
+        this.carape = carape;
+    }
+
+    public double getGustoca() {
+        return gustoca;
+    }
+
+    public void setGustoca(double gustoca) {
+        this.gustoca = gustoca;
+    }
+
+    public String getKratkaMajica() {
+        return kratkaMajica;
+    }
+
+    public void setKratkaMajica(String kratkaMajica) {
+        this.kratkaMajica = kratkaMajica;
+    }
+
+    public String getStilFrizure() {
+        return stilFrizure;
+    }
+
+    public void setStilFrizure(String stilFrizure) {
+        this.stilFrizure = stilFrizure;
+    }
+
+    public String getBojaOciju() {
+        return bojaOciju;
+    }
+
+    public void setBojaOciju(String bojaOciju) {
+        this.bojaOciju = bojaOciju;
+    }
+    
     
 }

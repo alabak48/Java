2d647aff20a5a834cd251ce323efa7099d850925
Anja Labak <anja.labak12345@gmail.com>
2022-04-13 21:08:19 +0200

    Ciklična matrica

---
+++ CiklicnaMatrica/src/main/java/ffos/ciklicnamatrica/Matrica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ffos.ciklicnamatrica;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author anja0
+ */
+public class Matrica {
+    public static void main(String[] args) {
+        int [] redak = new int[Integer.parseInt(JOptionPane.showInputDialog("Broj redaka"))];
+        int [] stupac = new int[Integer.parseInt(JOptionPane.showInputDialog("Broj stupaca"))];
+        
+  
+    }
+}

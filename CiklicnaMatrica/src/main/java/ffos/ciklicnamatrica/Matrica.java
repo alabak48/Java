8da14bfbe863aa9ff673bdfa635b555b79889fe8
Anja Labak <anja.labak12345@gmail.com>
2022-04-15 09:46:11 +0200

    Refactor

--- CiklicnaMatrica/src/main/java/ffos/ciklicnamatrica/Matrica.java
+++ CiklicnaMatrica/src/main/java/ffos/ciklicnamatrica/Matrica.java
@@ -4,10 +4,8 @@
  * and open the template in the editor.
  */
 package ffos.ciklicnamatrica;
-
-import java.awt.BorderLayout;
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,61 +14,67 @@ import java.util.Arrays;
 public class Matrica {
 
     public static void main(String[] args) {
-
-        int[][] niz = new int[5][5];
+        int red = 
+                  Integer.parseInt(JOptionPane.showInputDialog("Unesite broj redaka"));
+        int stupac = 
+                  Integer.parseInt(JOptionPane.showInputDialog("Unesite broj stupaca"));
+        int[][] niz = new int[red][stupac];
         int x = 0;
-        /* niz [4][4] = 1;
-       niz [4][3] = 2;
-       niz [4][2] = 3;
-       niz [4][1] = 4;
-       niz [4][0] = 5;
-         */
+       
+//2. dio - unos niza pomoću for petlji - MATRICA 5X5
 
         for (int i = 4; i >= 0; i--) {
             niz[4][i] = ++x;
         }
         
-        for (int i = 3; i >= 0; i--) {
-            niz[i][0] = ++x;
-        }
+            for (int i = 3; i >= 0; i--) {
+                niz[i][0] = ++x;
+            }
         
-        for (int i = 1; i<=4;i++) {
-            niz[0][i] = ++x;
-        }
+                for (int i = 1; i<=4;i++) {
+                    niz[0][i] = ++x;
+                }
         
-        for (int i = 1; i <= 3; i++) {
-            niz[i][4] = ++x;
-        }
+                    for (int i = 1; i <= 3; i++) {
+                        niz[i][4] = ++x;
+                    }
         
-        for (int i = 3; i >= 1; i--) {
-            niz[3][i] = ++x;
-        }
+                        for (int i = 3; i >= 1; i--) {
+                            niz[3][i] = ++x;
+                        }
         
-        for (int i = 2; i >= 1; i-- ) {
-            niz[i][1] = ++x;
-        }
+                        for (int i = 2; i >= 1; i-- ) {
+                            niz[i][1] = ++x;
+                        }
         
-        for (int i = 2; i<=3;i++) {
-            niz[1][i] = ++x;
-        }
+                    for (int i = 2; i<=3;i++) {
+                        niz[1][i] = ++x;
+                    }
         
-        for (int i = 2; i<= 2; i++) {
-            niz[i][3] = ++x;
-        }
+                for (int i = 2; i<= 2; i++) {
+                    niz[i][3] = ++x;
+                }
         
         for (int i = 2; i >= 2; i--) {
             niz[2][i] = ++x;
         }
         
-                for (int i =0;i<niz.length;i++) {
-                     System.out.println(Arrays.toString(niz[i]));
-                         }
         
-      
-       
+        
+        
+        
+        for (int i =0;i<niz.length;i++) {
+        System.out.println(Arrays.toString(niz[i]));
         }
-    }
+        
+//1. dio - ručni unos dvodimenzionalnog niza
 
+ /* niz [4][4] = 1;
+       niz [4][3] = 2;
+       niz [4][2] = 3;
+       niz [4][1] = 4;
+       niz [4][0] = 5;
+         */
         
 /*
        niz [3][0] = 6;
@@ -101,3 +105,5 @@ public class Matrica {
        
        niz [2][2] = 25;
  */
+  }
+    }
